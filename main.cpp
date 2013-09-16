0f0720331e591b3bbfa61ed21a93fa325949b1a2
unknown <clarkson@CLARKSON.na.qualcomm.com>
2013-09-16 16:47:18 -0400

    Using Windows to read all filenames instead of trying to read multiple with Magick.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,55 @@
 #include <cstdlib>
 #include <sstream>
+#include <vector>
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
 
 #include <Magick++.h>
 
 static const int kFrameRate = 24;
+static const char* kFormat = "%08d.png";
+
+typedef std::vector<std::string> Strings;
+typedef std::list<Magick::Image> ImageList;
+
+Strings GetFilenames(std::string const& dir, std::string const& ext)
+{
+  Strings filenames;
+
+  #ifdef _WIN32
+  WIN32_FIND_DATA find_data;
+  HANDLE h = FindFirstFile(std::string(dir + "\\*." + ext).c_str(), &find_data);
+  if (h != INVALID_HANDLE_VALUE) {
+    filenames.push_back(dir + "\\" + find_data.cFileName);
+    while (FindNextFile(h, &find_data)) {
+      filenames.push_back(dir + "\\" + find_data.cFileName);
+    }
+  }
+  #endif
+
+  return filenames;
+}
 
 int main(int argc, char* argv[])
 {
   Magick::InitializeMagick(*argv);
-  Magick::Image img;
-  img.read("C:\\Users\\Public\\Pictures\\Sample Pictures\\Chrysanthemum.jpg");
-  img.write("img0000.png");
+  
+  Strings filenames = GetFilenames("C:\\Users\\Public\\Pictures\\Sample Pictures", "jpg");
+  
+  for (size_t i = 0; i < filenames.size(); ++i) {
+    Magick::Image img;
+    img.read(filenames[i]);
 
+    char filename[0xFF];
+    sprintf(filename, kFormat, i);
+    img.write(filename);
+  }
+    
   std::stringstream ss;
   ss << "ffmpeg" 
-    << " -i img%04d.png"
+    << " -i " << kFormat
     << " -r " << kFrameRate 
     << " -c:v libx264"
     << " -pix_fmt yuv420p"
