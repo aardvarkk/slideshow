096d9a5dc26c2f09d068c2e12954afa65ea68dab
unknown <clarkson@CLARKSON.na.qualcomm.com>
2013-09-16 15:46:46 -0400

    Small demo that appears to be working. Making a .mp4 file that appears to show in Media Player.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
+#include <cstdlib>
+#include <sstream>
+
 #include <Magick++.h>
+
+static const int kFrameRate = 24;
 
 int main(int argc, char* argv[])
 {
   Magick::InitializeMagick(*argv);
   Magick::Image img;
   img.read("C:\\Users\\Public\\Pictures\\Sample Pictures\\Chrysanthemum.jpg");
-  img.write("helloworld.png");
+  img.write("img0000.png");
+
+  std::stringstream ss;
+  ss << "ffmpeg" 
+    << " -i img%04d.png"
+    << " -r " << kFrameRate 
+    << " -c:v libx264"
+    << " -pix_fmt yuv420p"
+    << " out.mp4"
+    << std::endl;
+  
+  int rc = system(ss.str().c_str());
+  return rc;
 }
