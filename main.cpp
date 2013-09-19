1787eb881698184a1afbf8045f8bef5b29e3bb42
Ian Clarkson <clarksonian@gmail.com>
2013-09-18 22:16:58 -0400

    Probably going to try switching to a more advanced space-taking system rather than the current simplistic approach.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <random>
 #include <sstream>
 #include <vector>
 
@@ -11,8 +12,14 @@
 #include <opencv2/opencv.hpp>
 
 static const int    kFrameRate = 24;
-static const double kTransitionTime = 1;
+static const double kTransitionPerc = 0.2; // This is a fraction of entire seconds per picture, so at 0.1, transition will take 10% of full picture time on each side
 static const char*  kFormat = "frames/%08d.png";
+static const int    kOutputW = 1920;
+static const int    kOutputH = 1080;
+static const double kMinScale = 0.9;
+static const double kAspect = static_cast<double>(kOutputW) / kOutputH;
+
+static std::tr1::mt19937 eng_;
 
 typedef std::vector<std::string> Strings;
 typedef std::vector<cv::Mat>     Images;
@@ -86,14 +93,90 @@ void ConcatenateMusic(Strings const& songs)
   ExecuteCommand(cmd.str());
 }
 
+void WriteFrame(cv::Mat const& img, int num)
+{
+  char filename[0xFF];
+  sprintf(filename, kFormat, num);
+  cv::imwrite(
+    filename, 
+    img
+  );
+}
+
+cv::Rect RandRect(cv::Mat const& img, double aspect, int min_dim)
+{
+  int x, y, w, h;
+  bool valid = false;
+
+  std::tr1::uniform_int<> min_d(min_dim, std::min(img.rows, img.cols) - 1);
+    
+  if (aspect > 1) {
+    h = min_d(eng_);
+    w = static_cast<int>(h * aspect + 0.5);
+  } else {
+    w = min_d(eng_);
+    h = static_cast<int>(w * aspect + 0.5);
+  }
+
+  std::tr1::uniform_int<> rnd_x(0, img.cols - w);
+  x = rnd_x(eng_);
+
+  std::tr1::uniform_int<> rnd_y(0, img.rows - h);
+  y = rnd_y(eng_);
+
+  return cv::Rect(x, y, w, h);
+}
+
+cv::Rect LinearInterpRect(cv::Rect const& start, cv::Rect const& end, int frame, int total)
+{
+  cv::Rect ret = start;
+  ret.x += (end.x - start.x) * frame / total;
+  ret.y += (end.y - start.y) * frame / total;
+  ret.width += (end.width - start.width) * frame / total;
+  ret.height += (end.height - start.height) * frame / total;
+  return ret;
+}
+
+// For a bad image, no fading occurs -- just sliding
+void BadImage(cv::Mat const& prv, cv::Mat const& cur, int to_use, int transition_frames, int* frame) 
+{
+  // Pick a start & end box of the "bad" aspect on the image
+  cv::Rect start_r = RandRect(cur, static_cast<double>(kOutputH) / kOutputW, std::min(cur.rows, cur.cols) * kMinScale);
+  cv::Rect end_r   = RandRect(cur, static_cast<double>(kOutputH) / kOutputW, std::min(cur.rows, cur.cols) * kMinScale);
+
+  // Required space after slide
+  int required_px = static_cast<int>(std::min(kOutputW, kOutputH) / kAspect);
+
+  // An output frame
+  cv::Mat comp = cv::Mat(kOutputW, kOutputH, CV_8UC3);
+
+  // How many frames we've used so far
+  int used = 0;
+
+  // Slide in
+  for (; used < transition_frames; ++used) {
+    int trans_loc = kOutputW - required_px * used / transition_frames;
+    cv::Rect crop = LinearInterpRect(start_r, end_r, used, to_use);
+
+    prv.copy
+
+    WriteFrame(comp, *frame++);
+  }
+
+  // Stay solid
+  for (; used < to_use; ++used) {
+    WriteFrame(comp, *frame++);
+  }
+}
+
 int main(int argc, char* argv[])
 {
   // NOTE: No spaces allowed in paths, because that's what FFMPEG demands!
-  Strings pictures = GetFilenames("C:\\Users\\Public\\Pictures\\Sample Pictures", "jpg");
-  Strings songs    = GetFilenames("C:\\Users\\clarkson\\Desktop\\slideshow\\bin", "mp3");
+  Strings pictures = GetFilenames("pictures", "jpg");
+  Strings songs    = GetFilenames("music", "mp3");
   
   // Stitch the music together into a WAV file (to help determine actual length)
-  ConcatenateMusic(songs);
+  //ConcatenateMusic(songs);
 
   // Get the duration of the music
   double duration;
@@ -105,30 +188,80 @@ int main(int argc, char* argv[])
 
   // We know the framerate, so convert the duration into a number of frames
   int frames = static_cast<int>(duration * kFrameRate + 0.5);
-  int frames_per_pic = frames / pictures.size();
+  double seconds_per_pic = duration / pictures.size();
+  double transition_seconds = seconds_per_pic * kTransitionPerc;
+  int transition_frames = static_cast<int>(transition_seconds * kFrameRate + 0.5);
 
-  Images images(pictures.size());
-  for (size_t i = 1; i < pictures.size(); ++i) {
-    images[i] = cv::imread(pictures[i]);
+  // We need to save some frames at the end for a fadeout
+  // Otherwise, every image is just a transition in, and then the image itself
+  frames -= transition_frames;
 
-    char filename[0xFF];
-    sprintf(filename, kFormat, i);
+  // Get a black image of the correct size
+  cv::Mat black(kOutputH, kOutputW, CV_8UC3);
+  black.setTo(0);
 
-    cv::imwrite(
-      filename, 
-      images[i] * 0.5 + images[i-1] * 0.5
-    );
+  // Start out saying our previous image is just black
+  cv::Mat prv = black;
+
+  // Start dealing with each picture
+  int frame = 0;
+  for (size_t i = 0; i < pictures.size(); ++i) {
+    cv::Mat cur = cv::imread(pictures[i]);
+    double aspect1 = static_cast<double>(cur.cols) / cur.rows;
+    double aspect2 = static_cast<double>(cur.rows) / cur.cols;
+
+    // Good image if our aspect is similar to the output aspect
+    bool good = (aspect1 > 1) == (kAspect > 1);
+
+    // How many frames will we give this image?
+    size_t start_frame = i * frames / pictures.size();
+    size_t end_frame   = (i+1) * frames / pictures.size() - 1;
+
+    if (good) {
+    } else {
+      BadImage(prv, cur, end_frame - start_frame + 1, transition_frames, &frame);
+    }
   }
+
+  // Do the first fade up
+  //int frame = 0;
+  //cv::Mat first = cv::imread(pictures.front());
+  //for (; frame < transition_frames; ++frame) {
+  //  double alpha = static_cast<double>(frame) / transition_frames;
+  //  WriteFrame(
+  //    alpha * first(cv::Rect(0, 0, kOutputW, kOutputH)) + (1-alpha) * black,
+  //    frame
+  //    );
+  //}
+
+  //// Go through all frames
+  //for (int i = 0; i < frames; ++i) {
+  //  double cur_time = static_cast<double>(i) / frames * duration;
+
+  //  static int prev_image_idx = 0;
+  //  int image_idx = i * pictures.size() / frames;
+
+  //  // We're starting a transition
+  //  if (image_idx != prev_image_idx) {
+  //  }
+
+  //}
+
+  //Images images(pictures.size());
+  //for (size_t i = 1; i < pictures.size(); ++i) {
+  //  images[i] = cv::imread(pictures[i]);
+
+  //}
     
-  std::stringstream ss;
-  ss << "ffmpeg -y" 
-    << " -i " << kFormat
-    << " -r " << kFrameRate 
-    << " -c:v libx264"
-    << " -pix_fmt yuv420p"
-    << " out.mp4"
-    << std::endl;
-  
-  int rc = system(ss.str().c_str());
-  return rc;
+  //std::stringstream ss;
+  //ss << "ffmpeg -y" 
+  //  << " -i " << kFormat
+  //  << " -r " << kFrameRate 
+  //  << " -c:v libx264"
+  //  << " -pix_fmt yuv420p"
+  //  << " out.mp4"
+  //  << std::endl;
+  //int rc = system(ss.str().c_str());
+
+  return EXIT_SUCCESS;
 }
